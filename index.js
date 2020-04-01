e24577290df6615ff9794a5b8bd9e17fc8124716
Stephanie <stephaniesmith12514@gmail.com>
2020-04-01 19:27:11 +0000

    Adds routing to index.js, fixes port for heroku server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const taskListRoute = require('./routes/taskList');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(taskListRoute);
+
+app.listen(port, () => console.log(`Listening on port ${port}.`));
