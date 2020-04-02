cf6da63cbffdc25eeefe0cb3d01553306e075843
Stephanie <stephaniesmith12514@gmail.com>
2020-04-02 04:38:21 +0000

    Adds a render to db path to display table

--- routes/taskList.js
+++ routes/taskList.js
@@ -13,16 +13,17 @@ router.get('/', (req, res) => {
 
 router.get('/db', async(req, res) => {
   try {
-    const client = await pool.connect()
+    const client = await pool.connect();
     const result = await client.query('SELECT * FROM tasks');
     const results = { 'results': (result) ? result.rows : null };
+    res.render('/db', results);
     client.release();
   }
   catch (err) {
     console.error(err);
     res.send("Error " + err);
   }
-})
+});
 
 
 router.get('/tasks', taskController.getAllTasks);
