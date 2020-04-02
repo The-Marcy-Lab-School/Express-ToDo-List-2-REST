3549275b25ccca77ab3284a297c0afcddea496cd
Enmanuel de la Nuez <enmanuel@et450.home>
2020-04-02 00:55:55 -0400

    needs connection fix

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const pg = require('pg');
+const { Pool } = require('pg');
 
 /* process.env.DATABASE_URL not working?
 const pool = new Pool({
@@ -6,12 +6,5 @@ const pool = new Pool({
   ssl: true,
 });
 */
-
-const connectionString = `postgres://fzktswgronzklb:06c1e360816845c7c6a31dccaad7a418b0b700fc9772fee1d0ceee81195bc34e@ec2-52-87-58-157.compute-1.amazonaws.com:5432/d9kveslllnenj0`;
-
-let connection;
-pg.connect(connectionString, function (err, client, done) {
-  connection = client.query;
-});
 
 module.exports = { query: (text, params) => connection(text, params) };
