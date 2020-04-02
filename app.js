67f8f99da1aefd14af3f5af1c4da20f569cb0c3d
EC2 Default User <ec2-user@ip-172-31-41-66.ec2.internal>
2020-04-02 13:55:53 +0000

    edited

--- app.js
+++ app.js
@@ -7,24 +7,12 @@ const Task = require('./controllers/Task');
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:null@localhost:5432/to_do',
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:null@/var/run/postgresql:5432/to_do',
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM tasks');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  });
 
 app.post('/users', (req, res) => {
    const {first_name, last_name, username} = req.body;
@@ -102,5 +90,18 @@ app.delete('/tasks/:id', (req, res) => {
             res.status(500).json({error: '500: Internal Server Error. Resource not created'});
         });
 });
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM tass');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  });
 
 app.listen(port, () => console.log(`Now listening on port ${port}`));
--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 const {Pool} = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: '/var/run/postgresql',
-  database: 'to_do',
-  password: '',
-  port: 5432
+  connectionString: process.env.DATABASE_URL,
 });
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: '/var/run/postgresql',
+//   database: 'to_do',
+//   password: '',
+//   port: 5432
+// });
 
 module.exports = {
     query: (text, params) => {
         return pool.query(text, params);
-    }
+    },
+    pool,
 };
