09544390601c3f90a6342bfcfd67aa1b56375305
paulsobers23 <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2020-04-02 12:51:52 +0000

    its deployed now but im unsure how to see the database

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
+const { Pool } = require('pg');
 const task = require('./controllers/task');
 
-const { Pool } = require('pg');
 const pool = new Pool();
 
 const app = express();
 
-const port =  process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 
@@ -25,20 +25,21 @@ app.put('/task/:id', task.updateTask);
 
 app.delete('/task/:id', task.deleteTask);
 
-app.get('/',(req,res)=>{
-    res.json('Hello World');
+app.get('/', (req, res) => {
+  res.json('Hello World');
 });
+
 app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM task');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM task');
+    const results = { results: (result) ? result.rows : null };
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send(`Error ${err}`);
+  }
 });
 
 app.listen(port, () => console.log(`Now listening on port ${port}`));
