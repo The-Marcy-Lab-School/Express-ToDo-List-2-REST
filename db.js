eaa928998b293afb76ba38383aa8c54820020757
Enmanuel de la Nuez <enmanuel@et450.home>
2020-04-01 23:26:43 -0400

    connect attempt, export query from pool

--- db.js
+++ db.js
@@ -5,15 +5,6 @@ const pool = new Pool({
   ssl: true,
 });
 
-const connectDb = async () => {
-  const connection = await pool.connect();
-  return connection.query;
-};
-
-const query = connectDb().catch((err) => {
-  console.error(err);
-});
-
 module.exports = {
-  query: (text, params) => query(text, params),
+  query: (text, params) => pool.query(text, params),
 };
