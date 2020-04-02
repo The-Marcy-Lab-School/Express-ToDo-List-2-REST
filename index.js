ca2ce5a10661064a1399fd5b2f3382891a7a7763
Stephanie <stephaniesmith12514@gmail.com>
2020-04-02 02:57:49 +0000

    Sets up pool for heroku

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const taskListRoute = require('./routes/taskList');
 const { Pool } = require('pg');
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
+
+pool.connect();
 
 
 const app = express();
@@ -15,18 +18,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(taskListRoute);
-
-app.get('./db', async(req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM todos');
-    const results = { 'results': (result) ? result.rows : null };
-    client.release();
-  }
-  catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}.`));
