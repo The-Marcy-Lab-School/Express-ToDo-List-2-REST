cc89a749b347d9af4c9c67917d1ca54badebcd27
paulsobers23 <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2020-04-02 13:54:35 +0000

    edit db.js

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'ec2-user',
-  host: '/var/run/postgresql',
-  database: 'tasklist',
-  password: null,
-  port: 5432,
-  connectionString: process.env.DATABASE_URL || 'postgresql://ec2-user:null@/var/run/postgresql:8080/tasklist'
-
+  // user: 'ec2-user',
+  // host: '/var/run/postgresql',
+  // database: 'tasklist',
+  // password: null,
+  // port: 5432,
+  connectionString: process.env.DATABASE_URL 
 });
 
 
