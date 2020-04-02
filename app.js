ac8a33dab20f364fbc3c0e7dc1fd41f2371f934a
Enmanuel de la Nuez <enmanuel@et450.home>
2020-04-01 23:46:46 -0400

    redirect to /todos

--- app.js
+++ app.js
@@ -10,18 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(todosRoute);
 
-app.get('/', (req, res) => {});
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM task;');
-    const results = { results: result ? result.rows : null };
-    res.render('pages/db', results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send('Error ' + err);
-  }
+app.get('/', (req, res) => {
+  res.redirect('/todos');
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
