f916db2786973ea6b4ee4808000cee8da52874f1
Stephanie <stephaniesmith12514@gmail.com>
2020-04-02 04:42:44 +0000

    Sends results instead of rendering

--- routes/taskList.js
+++ routes/taskList.js
@@ -16,12 +16,12 @@ router.get('/db', async(req, res) => {
     const client = await pool.connect();
     const result = await client.query('SELECT * FROM tasks');
     const results = { 'results': (result) ? result.rows : null };
-    res.render('/db', results);
+    res.send(results);
     client.release();
   }
   catch (err) {
     console.error(err);
-    res.send("Error " + err);
+    res.send(err);
   }
 });
 
