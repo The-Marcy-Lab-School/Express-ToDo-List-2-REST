7bfc1a860b3ee6ea58860114150e80d293b065b4
Enmanuel de la Nuez <enmanuel@et450.home>
2020-04-01 22:00:54 -0400

    add body parser middleware

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const todosRoute = require('./routes/todos');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(todosRoute);
 
