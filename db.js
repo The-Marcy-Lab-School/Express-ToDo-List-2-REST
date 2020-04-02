dff56a3e82e1a367223727c4e5ff16c6e56c1c70
Enmanuel de la Nuez <enmanuel@et450.home>
2020-04-02 00:46:38 -0400

    hard-code with temp credentials

--- db.js
+++ db.js
@@ -1,8 +1,17 @@
-const { Pool } = require('pg');
+const pg = require('pg');
 
+/* process.env.DATABASE_URL not working?
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
+*/
 
-module.exports = { query: (text, params) => pool.query(text, params) };
+const connectionString = `postgres://fzktswgronzklb:06c1e360816845c7c6a31dccaad7a418b0b700fc9772fee1d0ceee81195bc34e@ec2-52-87-58-157.compute-1.amazonaws.com:5432/d9kveslllnenj0`;
+
+let connection;
+pg.connect(connectionString, function (err, client, done) {
+  connection = client.query;
+});
+
+module.exports = { query: (text, params) => connection(text, params) };
