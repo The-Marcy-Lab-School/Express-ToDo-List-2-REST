82c0cfa04fc40b55c00c156ed3b14f06f84f41f3
Enmanuel de la Nuez <enmanuel@et450.home>
2020-04-01 22:03:55 -0400

    add create default params, switch update for toggle complete

--- models/Todos.js
+++ models/Todos.js
@@ -16,7 +16,12 @@ class Todos {
     return db.query(queryText, [taskId]);
   }
 
-  static create(taskName, taskDescription, isComplete, dueDate) {
+  static create(
+    taskName = null,
+    taskDescription = null,
+    isComplete = false,
+    dueDate = null,
+  ) {
     const queryText = `INSERT INTO task
     (task_name, task_description, is_complete, due_date)
     VALUES ($1, $2, $3, $4)`;
@@ -28,24 +33,15 @@ class Todos {
     ]);
   }
 
-  static update(taskId, taskName, taskDescription, isComplete, dueDate) {
-    const queryText = `UPDATE task
-    SET task_name = $2,
-    task_description = $3,
-    is_complete = $4,
-    due_date = $5
-    WHERE task_id = $1`;
-    return db.query(queryText, [
-      taskId,
-      taskName,
-      taskDescription,
-      isComplete,
-      dueDate,
-    ]);
+  static delete(taskId) {
+    const queryText = 'DELETE FROM task WHERE task_id = $1';
+    return db.query(queryText, [taskId]);
   }
 
-  static delete(taskId) {
-    const queryText = 'DELETE FROM task WHERE taskId = $1';
+  static toggleIsComplete(taskId) {
+    const queryText = `UPDATE task
+    SET is_complete = NOT is_complete
+    WHERE task_id = $1`;
     return db.query(queryText, [taskId]);
   }
 }
