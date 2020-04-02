3e2fc7fb9ed1dbc19477e48419cf434b458a60d5
Enmanuel de la Nuez <enmanuel@et450.home>
2020-04-01 23:39:55 -0400

    for heroku

--- app.js
+++ app.js
@@ -10,4 +10,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(todosRoute);
 
+app
+  .get('/', (req, res) => {})
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM task');
+      const results = { results: result ? result.rows : null };
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send('Error ' + err);
+    }
+  });
+
 app.listen(port, () => console.log(`Listening on port ${port}...`));
